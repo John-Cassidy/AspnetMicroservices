5825ce473a17a8ae32c63c2ac0e62beb6622c673
John Cassidy <jpcassidy@outlook.com>
2021-09-17 11:50:51 -0400

    Create Discount.API, PostgreSQL, pgadmin containers

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers {
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase {
+    }
+
+ 
+
+
+}
